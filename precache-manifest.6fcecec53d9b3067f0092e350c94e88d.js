026274576fc7a8eef5ecf91ada91bacd94ca30dd
Gourav-raj <gouravrj9654@gmail.com>
2021-02-22 21:33:51 +0530

    Updates

---
+++ precache-manifest.6fcecec53d9b3067f0092e350c94e88d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6abe33d03ed40a69110a0ecf0509f977",
+    "url": "/custee/index.html"
+  },
+  {
+    "revision": "7a795acb431ea60cb7e5",
+    "url": "/custee/static/css/main.baac3655.chunk.css"
+  },
+  {
+    "revision": "bb30344ace2ddeab62f3",
+    "url": "/custee/static/js/2.2ef7c36a.chunk.js"
+  },
+  {
+    "revision": "1cd206e4f52c8f356667f4e1ed8ba9f3",
+    "url": "/custee/static/js/2.2ef7c36a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a795acb431ea60cb7e5",
+    "url": "/custee/static/js/main.c0166209.chunk.js"
+  },
+  {
+    "revision": "d4c100ba5d92e2745161",
+    "url": "/custee/static/js/runtime-main.e422d3ee.js"
+  },
+  {
+    "revision": "29aa17cd2efa0e2a21449aeee9cb26b6",
+    "url": "/custee/static/media/colors.29aa17cd.png"
+  },
+  {
+    "revision": "475df4a01f569015eff31644da2c0f34",
+    "url": "/custee/static/media/crew_front.475df4a0.png"
+  },
+  {
+    "revision": "2516339970d710819585f90773aebe0a",
+    "url": "/custee/static/media/glyphicons-halflings.25163399.png"
+  }
+]);
